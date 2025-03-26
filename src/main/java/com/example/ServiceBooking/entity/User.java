d36c9861ffb950660c8c0188f5a3f6b4a285b833
PrateekSharma007 <btech.m2_66995@apsc.mait.ac.in>
2025-03-27 02:45:07 +0530

    user entity added

--- src/main/java/com/example/ServiceBooking/entity/User.java
+++ src/main/java/com/example/ServiceBooking/entity/User.java
@@ -1,4 +1,33 @@
 package com.example.ServiceBooking.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    private UserRole role;
 }
