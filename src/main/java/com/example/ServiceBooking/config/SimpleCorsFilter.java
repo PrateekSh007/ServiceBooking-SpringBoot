f3405a7a10f088f891caa253d24902ad4658f1b3
PrateekSharma007 <btech.m2_66995@apsc.mait.ac.in>
2025-04-01 03:02:57 +0530

    cors connection added

--- src/main/java/com/example/ServiceBooking/config/SimpleCorsFilter.java
+++ src/main/java/com/example/ServiceBooking/config/SimpleCorsFilter.java
@@ -1,4 +1,37 @@
 package com.example.ServiceBooking.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
 public class SimpleCorsFilter {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+
+        config.addAllowedOrigin("http://localhost:4200");
+
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("OPTIONS");
+
+
+        config.addAllowedHeader("*");
+
+
+        config.setAllowCredentials(true);
+
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
 }
